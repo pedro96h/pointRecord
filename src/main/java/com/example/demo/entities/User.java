0b17a65d9919f07af5d96ad5efc69cc740505c80
Pedro Oliveira <pedro96h@gmail.com>
2021-04-29 22:30:01 -0300

    class User

---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,70 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private String cpf;
+	
+	public User() {
+	}
+
+	public User(Long id, String name, String cpf) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		return true;
+	}
+}
