6a44067d4cea5fd7c585f7068df223acf111de5c
Pedro Oliveira <pedro96h@gmail.com>
2021-04-30 00:10:48 -0300

    service layer

---
+++ src/main/java/com/example/demo/servicies/UserService.java
@@ -0,0 +1,30 @@
+package com.example.demo.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return user.get();	
+		}else {
+			return null;
+		}
+	}
+}
