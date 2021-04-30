f1324e79db869ee83837d27025a7d69c6e47a1cd
Pedro Oliveira <pedro96h@gmail.com>
2021-04-29 22:44:31 -0300

    fix user class, h2 datebase and test profile

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,10 +2,16 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
