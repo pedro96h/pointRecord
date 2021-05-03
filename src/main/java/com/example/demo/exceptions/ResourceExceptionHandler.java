61ea94abf4c7f37d61d7f28458aa4f8134957026
Pedro Oliveira <pedro96h@gmail.com>
2021-05-03 00:39:40 -0300

    add exception to User GRUD

---
+++ src/main/java/com/example/demo/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.demo.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.demo.servicies.exception.DatabaseConstraintViolationException;
+import com.example.demo.servicies.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e,HttpServletRequest request){
+		String error = "resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseConstraintViolationException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseConstraintViolationException e,HttpServletRequest request){
+		String error = "Database constraint violation exception";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
---
+++ src/main/java/com/example/demo/servicies/exception/DatabaseConstraintViolationException.java
@@ -0,0 +1,10 @@
+package com.example.demo.servicies.exception;
+
+public class DatabaseConstraintViolationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseConstraintViolationException (String msg) {
+		super(msg);
+	}
+}
