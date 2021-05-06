1b9f9ed6cee679936856c4417143f2d7ed58d454
Pedro Oliveira <pedro96h@gmail.com>
2021-05-05 23:20:46 -0300

    new exception validationErrorException

--- src/main/java/com/example/demo/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/ResourceExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.servicies.exception.DatabaseConstraintViolationException;
 import com.example.demo.servicies.exception.ResourceNotFoundException;
+import com.example.demo.servicies.exception.ValidationErrorException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -27,6 +28,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> databaseException(DatabaseConstraintViolationException e,HttpServletRequest request){
 		String error = "Database constraint violation exception";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(ValidationErrorException.class)
+	public ResponseEntity<StandardError> validationErrorException(ValidationErrorException e,HttpServletRequest request){
+		String error = "Check violation exception";
+		HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
